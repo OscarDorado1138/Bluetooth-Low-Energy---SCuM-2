4f4b6fccc5840b13ccf81d8e80a1e291188385aa
OscarDorado1138 <OscarDorado1138@Berkeley.edu>
2017-07-07 13:11:53 -0700

    Sahar Send Packet Example

---
+++ send_string.c
@@ -0,0 +1,91 @@
+# include <stdio .h>
+# include <rt_misc .h>
+# include <stdlib .h>
+# include " Memory_map .h"
+
+// How to send a packet
+
+//1.) All packet data should be stored in a continuous, sequential, word-aligned
+//	section of data memory. This can be done by creating an array of chars with
+//	length equal to that of the packet data in bytes.
+
+//2.) Write the starting address of the packet data to the RFCONTROLLER_REG__TX_DATA_
+//	ADDR register.
+
+//3.) Write the length of the packet, in bytes, to the RFCONTROLLER_REG__PACK_LEN
+//	register.
+
+//4.) Set the TX_LOAD bit of the RFCONTROLLER_REG__CONTROL register, or use the
+//	radio timer to send a TX_LOAD signal/trigger. This will begin copying the
+//	packet data from memory.
+
+//5.) Wait for the packet data to nish copying. This can be indicated either by the
+//	TX_LOAD_DONE interrupt, or by monitoring/polling the TX_STATE bits of the
+//	RFCONTROLLER_REG__STATUS register.
+
+//6.) Set the TX_SEND bit of the RFCONTROLLER_REG__CONTROL register, or use the
+//	radio timer to send a TX_SEND signal/trigger. This will begin sending the
+// 	packet data.
+
+//7.) Wait for the packet to nish sending. This can be indicated by the TX_SEND-
+//	_DONE interrupt, or by monitoring/polling the TX_STATE bits of the RFCONTROLLER_
+// 	REG__STATUS register.
+
+
+int flag ;
+
+int main ( void ) 
+{
+	char send_packet [127];
+	char packet_length ;
+	char i;
+	flag = 0;
+	// Compose message
+	sprintf ( send_packet , " This is a test packet ");
+	// Set radio controller registers
+	RFCONTROLLER_REG__TX_DATA_ADDR = & send_packet [0];
+	RFCONTROLLER_REG__TX_PACK_LEN = 21;
+	// Set radio timer registers
+	RFTIMER_REG__COMPARE0 = 500000; // Set compare unit 0 value to 1s
+	RFTIMER_REG__COMPARE0_CONTROL = 0xB; // Binary value 001011
+	// Enable compare unit
+	// Enable interrupt
+	// Enable the TX_SEND output to radio
+	RFTIMER_REG__CAPTURE1_CONTROL = 0x9; // Binary value 000001001
+	// Enable interrupt
+	// Enable TX_SFD_DONE input
+	RFTIMER_REG__CAPTURE2_CONTROL = 0x11; // Binary value 000010001
+	// Enable interrupt
+	// Enable TX_SEND_DONE input
+	// Start sending packet with TX_LOAD
+	RFCONTROLLER_REG__CONTROL = 0x01;
+	// Start the timer , enable interrupts , and reset counter
+	RFTIMER_REG__CONTROL = 0x7;
+	// Wait until packet is finished sending and
+	// the radio timer prints all values
+	while ( flag == 0) {}
+		printf (" done \n");
+}
+
+168
+void RFTIMER_ISR()
+{
+	// Read the interrupt register
+	unsigned int interrupt = RFTIMER_REG__INT ;
+	// Respond to different interrupt sources
+	if ( interrupt & 0 x00000001 ) printf (" Telling radio to send packet with TX_SEND \n
+	");
+	if ( interrupt & 0 x00000200 ) printf (" TX SFD DONE at 0x%x\n",
+	RFTIMER_REG__CAPTURE1 );
+	if ( interrupt & 0 x00000400 ) { printf (" TX SEND DONE at 0x%x\n",
+	RFTIMER_REG__CAPTURE2 ); flag = 1; }
+	// Clear the interrupt register
+	RFTIMER_REG__INT_CLEAR = interrupt ;
+}
+
+
+
+
+
+
+
